c77e375e7190fc250c232c082435a2d51d0b77e4
Marcos Maues <yboostx@gmail.com>
2025-03-06 23:10:44 -0300

    06.03.25

---
+++ aula-c#/ExercicioAula/Exercicio1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Desenvolver um algoritmo para receber 1000 valores automaticamente dentro de um vetor e ordenar do menor para o maior.
+
+1. Desenvolver o algoritmo de ordenação (Bubble Sort);
+2. Utilizar uma função em C# para ordenação;*/
+
+int[] vetor = new int[10];
+int aux;
+int contador = 0;
+Random numAleatorio = new Random();
+for(int i = 0; i < vetor.Length; i++){
+    vetor[i] = numAleatorio.Next(1, 20);
+}
+
+for(int i = 0; i < vetor.Length ; i++){
+    Console.Write(vetor[i] + " ");
+}
+do{
+    contador = 0;
+for(int i = 0; i < vetor.Length - 1; i++){
+    if(vetor[i]>vetor[i + 1]){
+        aux = vetor[i];
+        vetor[i] = vetor[i + 1];
+        vetor[i + 1] = aux; 
+        contador++;
+    }else{
+        continue;
+    }
+}
+}while(contador != 0);
+Console.WriteLine(" ");
+for(int i = 0; i < vetor.Length ; i++){
+    Console.Write(vetor[i] + " ");
+}
---
+++ aula-c#/novoProjeto/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Digite a sua idade:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if(age >= 18){
+    Console.WriteLine("Maior de idade");
+}else{
+    Console.WriteLine("Menor de idade");
+}
